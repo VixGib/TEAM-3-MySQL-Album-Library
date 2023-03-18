51cf64ad564e43c6ba511a466329da5e4784de12
vicki gibbison <vickigibbison@hotmail.com>
2023-03-18 09:26:03 +0000

    user stories

--- Untitled.sql
+++ Untitled.sql
@@ -1,1 +1,18 @@
 -- hello TEAM 3!!
+
+-- User stories
+-- As a Member I want to be able to:
+	-- Borrow an Album
+	-- Search for an Album by Artist
+	-- Search for any Album by Any Name
+	-- View my History
+	-- Check my Due Dates
+    
+-- As the Lending system I want to be able to:
+	-- Check Availability
+    -- To see Whos is Borrowing what
+    -- To be able to Contact a Member
+    
+    
+
+
