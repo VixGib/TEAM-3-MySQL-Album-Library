1906c1b017f31252b581bfe6372e67362c4e2af2
vicki gibbison <vickigibbison@hotmail.com>
2023-03-19 18:18:59 +0000

    Faye, Vicki, Rachel data

--- Vicki Team 3 Library.sql
+++ Vicki Team 3 Library.sql
@@ -84,7 +84,7 @@ insert into record_label values(4, "EMI");
 insert into record_label values(5, "Columbia");
 insert into record_label values(6, "PHILIPS");
 
--- Record label data
+-- Vicki's Record label data
 
 insert into record_label values(7, "RCA Records");
 insert into record_label values(8, "LA Face Records");
@@ -93,22 +93,34 @@ insert into record_label values(10, "Deconstruction Records");
 insert into record_label values(11, "Fly Eye Records");
 insert into record_label values(12, "Columbia");
 
+-- Rachel's Record Label
+insert into record_label values(13, "Roc Nation Records");
+insert into record_label values(14, "Chrysalis Records Limited");
+insert into record_label values(15, "More Alarming Records");
+insert into record_label values(16, "Silvertone Records Limited");
+insert into record_label values(17, "Westbury Road Roc Nation");
 
--- Fays's Artists
+
+-- Fays's Artists data
 insert into artist values(1, "Frankie", "Valli", "American");
 insert into artist values(2, "Toots and the Maytals", "Toots and the Maytals", "Jamaican");
 insert into artist values(3, "George", "Ezra", "English");
 insert into artist values(4, "Diana", "Ross", "American");
 insert into artist values(5, "Johnny", "Cash", "American");
 
--- Artist data
+-- Vicki's Artist data
 
 insert into artist values(6, "Pink", "Pink", "American");
 insert into artist values(7, "Phil", "Collins", "English");
 insert into artist values(8, "Take That", "Take That", "English");
 insert into artist values(9, "Calvin", "Harris", "Scotish");
 
--- Faye's Album
+-- Rachel Artist data
+insert into artist values(10, "The Stone Roses", "The Stone Roses");
+insert into artist values(11, "Rihanna", "Rihanna");
+insert into artist values(12, "Laura", "Marling");
+
+-- Faye's Album data
 insert into album values(1, "Working my way back to you", 1, 1, 1, 2011);
 insert into album values(2, "True Love", 2, 4, 2, 2004);
 insert into album values(3, "Gold Rush Kid", 3, 3, 3, 2022);
@@ -119,7 +131,7 @@ insert into album values(7, "Staying at Tamara's", 3, 3, 5, 2018);
 insert into album values(8, "Solo", 1, 1, 6, 1967);
 insert into album values(9, "Workin' overtime", 4, 5, 4, 1989);
 
--- Album data
+-- Vicki's Album data
 
 insert into album values(10, "All I Know So Far", 6, 1, 7, 2021);
 insert into album values(11, "Fun House", 6, 1, 8, 2008);
@@ -127,14 +139,22 @@ insert into album values(12, "No Jacket Required", 7, 1, 9, 1985);
 insert into album values(13, "Never Forget", 8, 1, 7, 2005);
 insert into album values(14, "18 Months", 9, 6, 10, 2012);
 
--- Faye's Customer
+-- Rachel's Album data
+insert into album values(15, "The Stone Roses" , 10, 8, 16, 1989);
+insert into album values(16, "Loud" , 11, 7, 13, 1989);
+insert into album values(17, "Anti" , 11, 7, 17, 1989);
+insert into album values(18, "Song for our Daughter", 12, 10, 15, 1989);
+insert into album values(19, "Semper Femina", 12, 10, 14, 1989);
+
+
+-- Faye's Customer data
 insert into customer values(1, "Faye", "Quinn", "fayequinn@email.com", "07836233821", "4 Blossom Hill", "WF4 6HT", "2018-03-12");
 insert into customer values(2, "Bobby", "Smith", "bobbysmith@email.com", "07865752816", "87 Cherry Tree Drive", "WF5 6F1", "2018-04-18");
 insert into customer values(3, "Carl", "Turner", "carlturner@email.com", "07652988789", "21 Magnolia Way", "WF3 3LT", "2019-01-05");
 insert into customer values(4, "William", "Brooks", "williambrooks@warmmail.com", "07885990823", "43 Daffodil Drive", "LS3 0LY", "2014-07-16");
 insert into customer values(5, "Louis", "Potter", "louispotter@email.com", "07889776542", "12 Tulip Close", "WF2 8LT", "2015-09-09");
 
--- Customer data
+-- Vicki's Customer data
 
 insert into customer values(6, "Vicki", "Gibbison", "vickigibbison@hotmail.com", "07967806688", "85 Shakespeare Drive", "SK82DQ", "2011-08-11");
 insert into customer values(7, "Phil", "Brown", "philbrown@sky.com", "07856935628", "52 Ash Road", "WA167RP", "2014-02-28");
@@ -142,3 +162,8 @@ insert into customer values(8, "Carloine", "Pickle", "carolinepickle@skyemail.co
 insert into customer values(9, "Andrew", "Wallis", "andrewwallis@aol.com", "07784602275", "1 Hill Avenue", "WA3 5PZ", "2016-11-12");
 insert into customer values(10, "Lois", "Travis", "loistravis@gmail.com", "07620853399", "14 Parsonage Close", "M28 3PT", "2019-01-30");
 
+-- Rachel' Customer data
+insert into customer values(11, "John", "Barnes", "jb@gmail.com", "07783888555", "5 The Row", "BL1 5LJ", 2022-06-21);
+insert into customer values(12, "Jenna", "Wade", "jennagirl@gmail.com", "07885101295", "15 Walton avenue", "TS5 7RN", 2023-01-21);
+insert into customer values(13, "Evelyn", "Boomer", "eve45@btconnect.com", "07234661660", "Rose Cottage", "HX6 1DG", 2023-01-30);
+insert into customer values(14, "Dave", "Glynn", "dglynn@yahoo.co.uk", "07885101295", "15 Walton avenue", "TS5 7RN", 2022-01-21);
