c46016e2d9734815d2254fb9e00b32f342bc5cee
Faye <fayequinn88@hotmail.com>
2023-03-31 21:47:19 +0100

    more work on dbScript

--- Vicki/libraryDBScript.py
+++ Vicki/libraryDBScript.py
@@ -3,7 +3,7 @@ from sqlalchemy import text
 
 # using root user is not a great idea!!
 user = 'root'
-password = ''
+password = 'password'
 host = '127.0.0.1'
 port = 3306
 database = 'Library'
@@ -60,19 +60,19 @@ engine = get_connection()
 #         print(f"First Name: {row.first_name}  \nLast Name: {row.last_name} \nemail: {row.email} \nMobile Num: {row.mobile_num} \nJoin Date: {row.join_date}")
 
 
-#print('************* join *********')
-fname = input("What is the customer's first name? ")
-lname = input("What is the customer's last name? ")
-
-with engine.connect() as conn:
-    result = conn.execute(
-        text("""select c.first_name cust_first_name, c.last_name cust_last_name, c.cust_id, l.loan_album_id , l.due_back_date
-        from customer c inner join loan_procedure l
-        on c.cust_id = l.loan_cust_id where c.first_name = :fname and c.last_name = :lname"""),
-        {"fname": fname, "lname" : lname}
-    )
-    for row in result:
-        print(f"First Name: {row.cust_first_name}  \nLast Name: {row.cust_last_name} \nCustomer Id: {row.cust_id} \nAlbum Id: {row.loan_album_id} \nDue Back Date: row.{row.due_back_date}")
+# #print('************* join *********')
+# fname = input("What is the customer's first name? ")
+# lname = input("What is the customer's last name? ")
+#
+# with engine.connect() as conn:
+#     result = conn.execute(
+#         text("""select c.first_name cust_first_name, c.last_name cust_last_name, c.cust_id, l.loan_album_id , l.due_back_date
+#         from customer c inner join loan_procedure l
+#         on c.cust_id = l.loan_cust_id where c.first_name = :fname and c.last_name = :lname"""),
+#         {"fname": fname, "lname" : lname}
+#     )
+#     for row in result:
+#         print(f"First Name: {row.cust_first_name}  \nLast Name: {row.cust_last_name} \nCustomer Id: {row.cust_id} \nAlbum Id: {row.loan_album_id} \nDue Back Date: row.{row.due_back_date}")
 
 
 # with engine.connect() as conn:
@@ -80,3 +80,7 @@ with engine.connect() as conn:
 #         text("""update manager set first_name = 'Christmas' where man_id = 3""")
 #     )
 #     conn.commit()
+
+
+
+
