53a5ae9a8951bdaf0018ebe336496b47ddcba640
vicki gibbison <vickigibbison@hotmail.com>
2023-04-02 22:53:20 +0100

    Session query within session query

--- Vicki/libraryDBConnect/application/libraryDBScriptAlchemy.py
+++ Vicki/libraryDBConnect/application/libraryDBScriptAlchemy.py
@@ -28,7 +28,7 @@ engine = get_connection()
 Session = sessionmaker(bind=engine)
 
 session = Session()
-
+# MARTINA'S CODE
 # manager = session.query(Manager).filter_by(first_name="Santa").first()
 # print(manager.first_name, manager.last_name, manager.man_id, manager.employees)
 # for e in manager.employees:
@@ -36,21 +36,24 @@ session = Session()
 #     print(e.last_name)
 
 
-customer = session.query(Customer).filter_by(first_name="Vicki").first()
-print("Customer Name:", customer.first_name, customer.last_name, "\nLoan id:", customer.cust_id)
-for e in customer.loans:
-    print("Issue Date: ", e.issue_date)
-    print("Due Back Date: ", e.due_back_date)
-    album = session.query(Album).filter_by(album_id=e.loan_album_id).first()
-    print("Album due bask:",album.album_name)
+# # SEARCH FOR ALBUM NAME AND LOAN AND DATES BY CUSTOMER FIRST NAME
+# customer = session.query(Customer).filter_by(first_name="Vicki").first()
+# print("Customer Name:", customer.first_name, customer.last_name, "\nLoan id:", customer.cust_id)
+# for e in customer.loans:
+#     print("Issue Date: ", e.issue_date)
+#     print("Due Back Date: ", e.due_back_date)
+#     album = session.query(Album).filter_by(album_id=e.loan_album_id).first()
+#     print("Album due bask:",album.album_name)
 
 
-#
+## SEARCH GENRE FOR ALL POP ALBUMS AND RETURN ALBUM NAME AND YEAR
 # genre = session.query(Genre).filter_by(genre_name="Pop").first()
 # print(len(genre.albumsG), genre.genre_name, "Albums")
 # for e in genre.albumsG:
 #     print(e.album_name,":", e.album_year)
 
+
+## SEARCH ARTIST BY ARTIST FIRST NAME AND PRINT ALL ALBUMS FOR THAT ARTIST
 # artist = session.query(Artist).filter_by(first_name="pink").first()
 # if artist.first_name == artist.last_name:
 #     print("Here are the albums by", artist.first_name,":")
@@ -60,6 +63,7 @@ for e in customer.loans:
 #     print(e.album_name)
 
 
+## SEARCH GENRE FOR ALL POP ALBUMS AND RETURN HOW MANY ALBUMS, THE ALBUM NAME, YEAR AND ARTIST NAME
 # genre = session.query(Genre).filter_by(genre_name="Pop").first()
 # print("There are", len(genre.albumsG), genre.genre_name, "Albums")
 # for e in genre.albumsG:
@@ -71,7 +75,20 @@ for e in customer.loans:
 #         print("Artist:", artist.first_name, artist.last_name, "\n")
 
 
+# SEARCH FOR ALL ALBUMS BY AMERICAN ARTISTS
+# artist = session.query(Artist).filter_by(nationality="American").all()
+# print("There are", len(artist), "Artists")
+# for e in artist:
+#     if e.first_name == e.last_name:
+#         print("\nArtist:", e.first_name)
+#     else:
+#         print("\nArtist:", e.first_name, e.last_name)
+#     album = session.query(Album).filter_by(artist_id=e.art_id).all()
+#     for a in album:
+#         print("Album:", a.album_name)
 
+
+## MARTINA'S CODE
 # # manager object
 # manager = Manager(first_name="Man1", last_name="Man2")
 # # inserting it into the database
@@ -79,11 +96,19 @@ for e in customer.loans:
 # # don't forget to commit!
 # session.commit()
 
+
+## ADD ANOTHER GENRE
 # genre = Genre(genre_name="Opera")
 # session.add(genre)
 # session.commit()
 
+
+## ADD A NEW CUSTOMER
 # customer = Customer(first_name="David", last_name="Beckham", email="davidbeckham@email.com", mobile_num="07658395584", address="43 Football Road", postcode="LL2 5GT", join_date="2023-04-01")
 # session.add(customer)
 # session.commit()
 
+
+## DELETE A GENRE BY ID
+# session.query(Genre).filter_by(genre_id=16).delete()
+# session.commit()
